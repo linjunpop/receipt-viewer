0caf0d47f2cf9c655239ee18ada291d4a39b88bc
Jun Lin <linjunpop@gmail.com>
2018-02-25 19:06:14 +0800

    Added turbolinks.

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,8 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import 'phoenix_html';
+import 'turbolinks';
 
 // Import local files
 //
@@ -19,3 +20,5 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+Turbolinks.start();
